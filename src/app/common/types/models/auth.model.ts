9471c45dfb3688cf93f824d72465274b63f64867
OmarKnows <omarknows98@gmail.com>
2024-08-12 16:20:14 +0300

    prepared auth service and auth model

---
+++ src/app/common/types/models/auth.model.ts
@@ -0,0 +1,6 @@
+export interface IAuth {
+  _id: string;
+  email: string;
+  accessToken: string;
+  refreshToken: string;
+}
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { EAPI } from '../constants/eapi';
+import { catchError, throwError } from 'rxjs';
+import { IAuthResponse } from '../common/types/shared.types';
+import { IAuth } from '../common/types/models/auth.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsersService {
+  private httpClient = inject(HttpClient);
+
+  login(email: string, password: string) {
+    return this.httpClient
+      .post<IAuthResponse<IAuth>>(EAPI.AUTH + 'login', {
+        email,
+        password,
+      })
+      .pipe(
+        catchError((error: any) => {
+          console.log(error);
+          return throwError(
+            () => new Error('An error occurred while logging in')
+          );
+        })
+      );
+  }
+}
