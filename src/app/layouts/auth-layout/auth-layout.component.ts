98bca09b5d8c4649314be765490633bad3245977
OmarKnows <omarknows98@gmail.com>
2024-08-12 12:30:20 +0300

    finished auth layout ui

--- src/app/layouts/auth-layout/auth-layout.component.ts
+++ src/app/layouts/auth-layout/auth-layout.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-auth-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, MatGridListModule],
   templateUrl: './auth-layout.component.html',
 })
-export class AuthLayoutComponent {}
+export class AuthLayoutComponent {
+  loginBannerPath = 'assets/images/login-banner.webp';
+}
