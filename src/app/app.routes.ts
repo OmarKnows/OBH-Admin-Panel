7b89087f947b50392d15c7e022c26b8ebda50fe7
OmarKnows <omarknows98@gmail.com>
2024-08-12 12:11:29 +0300

    integrated all routes into router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { authRoutes } from './routes/auth.routes';
+import { branchRoutes } from './routes/branch.routes';
+import { caseTypeRoutes } from './routes/case-types.routes';
+import { courtRoutes } from './routes/court.routes';
+import { employeeRoutes } from './routes/employee.routes';
+import { regionRoutes } from './routes/region.routes';
+import { roleRoutes } from './routes/role.routes';
+import { serviceRoutes } from './routes/serivce.routes';
+import { userRoutes } from './routes/user.routes';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { NotFoundComponent } from './common/components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  ...authRoutes,
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      ...employeeRoutes,
+      ...userRoutes,
+      ...roleRoutes,
+      ...serviceRoutes,
+      ...regionRoutes,
+      ...branchRoutes,
+      ...courtRoutes,
+      ...caseTypeRoutes,
+    ],
+  },
+  { path: '**', component: NotFoundComponent },
+];
