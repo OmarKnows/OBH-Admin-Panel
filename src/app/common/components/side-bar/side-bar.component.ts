5364a51a306d59219ef30cb45ca8031f984f6e3b
OmarKnows <omarknows98@gmail.com>
2024-08-12 15:10:44 +0300

    finished sidebar ui

---
+++ src/app/common/components/side-bar/side-bar.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import {
+  IsActiveMatchOptions,
+  RouterLink,
+  RouterLinkActive,
+} from '@angular/router';
+import { ROUTES } from '../../../constants/routes';
+import { Router } from '@angular/router';
+import { INavItem } from '../../types/interfaces/nav-item.interface';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-side-bar',
+  standalone: true,
+  imports: [MatIconModule, RouterLink, RouterLinkActive, CommonModule],
+  templateUrl: './side-bar.component.html',
+})
+export class SideBarComponent {
+  constructor(private router: Router) {}
+
+  logoSrc = 'assets/logos/logo-white.svg';
+  navItems: INavItem[] = [
+    { icon: 'work', label: 'Employees', route: ROUTES.EMPLOYEES },
+    { icon: 'person', label: 'Users', route: ROUTES.USERS },
+    { icon: 'shield', label: 'Roles', route: ROUTES.ROLES },
+    { icon: 'room_service', label: 'Services', route: ROUTES.SERVICES },
+    { icon: 'location_pin', label: 'Regions', route: ROUTES.REGIONS },
+    { icon: 'domain', label: 'Branches', route: ROUTES.BRANCHES },
+    { icon: 'account_balance', label: 'Courts', route: ROUTES.COURTS },
+    { icon: 'cases', label: 'Case Types', route: ROUTES.CASE_TYPES },
+  ];
+  logoutNav: INavItem = {
+    icon: 'logout',
+    label: 'Logout',
+    route: `${ROUTES.AUTH}/${ROUTES.LOGIN}`,
+  };
+  myMatchOptions: IsActiveMatchOptions = {
+    queryParams: 'ignored',
+    matrixParams: 'exact',
+    paths: 'exact',
+    fragment: 'exact',
+  };
+
+  isActive(route: string): boolean {
+    return this.router.isActive(
+      this.router.createUrlTree([route]),
+      this.myMatchOptions
+    );
+  }
+}
--- src/app/layouts/main-layout/main-layout.component.ts
+++ src/app/layouts/main-layout/main-layout.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { SideBarComponent } from '../../common/components/side-bar/side-bar.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, MatSidenavModule, SideBarComponent],
   templateUrl: './main-layout.component.html',
 })
 export class MainLayoutComponent {}
