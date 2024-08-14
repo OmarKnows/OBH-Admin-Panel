070666a20511653fbf4c25bd0fd7d8d58af45bef
OmarKnows <omarknows98@gmail.com>
2024-08-14 11:40:54 +0300

    integrated global loading state into auth layout

--- src/app/layouts/auth-layout/auth-layout.component.ts
+++ src/app/layouts/auth-layout/auth-layout.component.ts
@@ -1,18 +1,26 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { ELocalStorage } from '../../constants/local-storage';
-import { ROUTES } from '../../constants/routes';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-auth-layout',
   standalone: true,
-  imports: [RouterOutlet, MatGridListModule],
+  imports: [RouterOutlet, MatGridListModule, MatProgressBarModule, AsyncPipe],
   templateUrl: './auth-layout.component.html',
 })
 export class AuthLayoutComponent implements OnInit {
   private router = inject(Router);
+  isLoading$: Observable<boolean>;
   loginBannerPath = 'assets/images/login-banner.webp';
+
+  constructor(private store: Store<{ isLoading: boolean }>) {
+    this.isLoading$ = store.select('isLoading');
+  }
 
   ngOnInit(): void {
     const accessToken = localStorage.getItem(ELocalStorage.ACCESS_TOKEN);
